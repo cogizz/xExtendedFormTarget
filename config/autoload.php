40f43f41aea7a3c5ac500f56e81278018d770210
Jens Buchta <j.buchta@revision6.de>
2014-03-21 12:36:50 +0100

    add autoload for contao 3 compatibility
    add autoload.ini / .php to make the extension compatible with contao 3

---
+++ config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package XExtendedFormTarget
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ExtendedFormTarget' => 'system/modules/xExtendedFormTarget/ExtendedFormTarget.php',
+));
